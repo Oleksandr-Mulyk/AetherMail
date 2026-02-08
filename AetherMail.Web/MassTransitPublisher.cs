1c051beeb6ab4cd18abd398b3a171112c7d043d4
Oleksandr Mulyk <alex@mulyk.net>
2026-02-08 13:20:38 +0100

    Implemented MassTransitPublisher

--- AetherMail.Web/MassTransitPublisher.cs
+++ AetherMail.Web/MassTransitPublisher.cs
@@ -5,9 +5,7 @@ namespace AetherMail.Web
 {
     public class MassTransitPublisher(IPublishEndpoint publishEndpoint) : IMessagePublisher
     {
-        public Task PublishAsync<T>(T message, CancellationToken ct = default) where T : class
-        {
-            throw new NotImplementedException();
-        }
+        public async Task PublishAsync<T>(T message, CancellationToken ct = default) where T : class
+            => await publishEndpoint.Publish(message, ct);
     }
 }
