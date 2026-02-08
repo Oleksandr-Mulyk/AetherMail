5d7850e8d698fdcddb395f06f8dd3671a2c9e161
Oleksandr Mulyk <alex@mulyk.net>
2026-02-08 12:29:54 +0100

    add unit test project and initial contract smoke test

---
+++ AetherMail.Tests/ContractTests.cs
@@ -0,0 +1,19 @@
+﻿using AetherMail.Contracts;
+using FluentAssertions;
+
+namespace AetherMail.Tests
+{
+    public class ContractTests
+    {
+        [Fact]
+        public void SendEmailCommand_ShouldRetainProperties()
+        {
+            // Arrange & Act
+            var command = new SendEmailCommand("test@mail.com", "Hello", "Body");
+
+            // Assert
+            command.To.Should().Be("test@mail.com");
+            command.Subject.Should().Be("Hello");
+        }
+    }
+}
